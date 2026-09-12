df56cd9246585bdf23a98dcd5e75e3346d64e6b2
Hanuman <75466721+hanusahani4@users.noreply.github.com>
2026-09-12 10:36:44 +0530

    Add homepage configuration repository

---
+++ wissfind-fullstack-seller-admin-image-fixed-build/backend/src/main/java/com/wissfind/marketplace/repo/HomepageConfigurationRepository.java
@@ -0,0 +1,7 @@
+package com.wissfind.marketplace.repo;
+
+import com.wissfind.marketplace.entity.HomepageConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HomepageConfigurationRepository extends JpaRepository<HomepageConfiguration, Long> {
+}
