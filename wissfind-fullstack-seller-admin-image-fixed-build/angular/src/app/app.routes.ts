11caea8f45a1f433c11cf30060d6a2603ad7271c
Hanuman <75466721+hanusahani4@users.noreply.github.com>
2026-09-16 22:25:05 +0530

    Enable customer product variant selector

--- wissfind-fullstack-seller-admin-image-fixed-build/angular/src/app/app.routes.ts
+++ wissfind-fullstack-seller-admin-image-fixed-build/angular/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { razorpayCheckoutGuard } from './core/razorpay-checkout.guard';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home.component').then(m => m.HomeComponent) },
-  { path: 'product/:id', loadComponent: () => import('./pages/product-detail.component').then(m => m.ProductDetailComponent) },
+  { path: 'product/:id', loadComponent: () => import('./pages/product-detail-variant-wrapper.component').then(m => m.ProductDetailVariantWrapperComponent) },
   { path: 'login', loadComponent: () => import('./pages/login.component').then(m => m.LoginComponent) },
   { path: 'signup', loadComponent: () => import('./pages/signup.component').then(m => m.SignupComponent) },
   { path: 'forgot-password', loadComponent: () => import('./pages/forgot-password.component').then(m => m.ForgotPasswordComponent) },
